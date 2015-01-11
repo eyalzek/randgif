8b36b88bd2255993ce2c3253d95627e1ced58626
eyalzek <eyal.ez@gmail.com>
2015-01-11 12:51:46 +0200

    added exception handling

--- randgif.py
+++ randgif.py
@@ -6,7 +6,10 @@ import requests
 
 def main(argv):
     r = requests.get(get_url("+".join([str(arg) for arg in argv[1:]])))
-    webbrowser.open(random.choice(r.json()["data"])["images"]["original"]["url"])
+    try:
+        webbrowser.open(random.choice(r.json()["data"])["images"]["original"]["url"])
+    except IndexError:
+        print("No gifs found for this query")
 
 def get_url(query):
     return "http://api.giphy.com/v1/gifs/search?q={}&api_key=dc6zaTOxFJmzC".format(query)
